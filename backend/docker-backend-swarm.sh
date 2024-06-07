eac38a8689111f81d35792be109a90991704c969
Leon Junio Martins Ferreira <juniohuy2gamer@gmail.com>
2024-06-07 12:18:15 -0300

    Fix backend stack deploy

--- backend/docker-backend-swarm.sh
+++ backend/docker-backend-swarm.sh
@@ -1,6 +1,8 @@
 # start backend swarm cluster
 echo Starting swarm cluster configuration!
 
+docker swarm init
+
 export $(cat .env) > /dev/null 2>&1
 
 # creating network overlay
@@ -8,9 +10,5 @@ docker network create --driver overlay load_balancer_network
 docker network create --driver overlay backend
 docker network create --driver overlay kafka_net
 
-# start docker swarm
-docker swarm init
-
-# deploy app stacks
-docker stack deploy -c docker-compose.yml --resolve-image always backend
+docker stack deploy -c docker-compose.yml --resolve-image always --with-registry-auth backend
 docker service ls
